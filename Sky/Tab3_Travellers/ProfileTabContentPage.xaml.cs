8549a3ed39f00afdfbd3f5dce5209c25d466332a
Robert Badea <badea.robert@ymail.com>
2020-01-01 22:01:08 +0200

    Updates to three tabs

--- Sky/Tab3_Travellers/ProfileTabContentPage.xaml.cs
+++ Sky/Tab3_Travellers/ProfileTabContentPage.xaml.cs
@@ -12,27 +12,65 @@ namespace Sky.Tab3_Travellers
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ProfileTabContentPage : ContentPage
     {
+        private enum Page
+        {
+            About = 0, 
+            Favourites = 1,
+            Gallery = 2
+        };
+        private Page currentPage = Page.Gallery;
         public ProfileTabContentPage()
         {
             InitializeComponent();
+            LoadContent(Page.About);
         }
 
+        private View Create(Page newPage)
+        {
+            switch (currentPage)
+            {
+                case Page.About: return new TabAbout_ContentView();
+                case Page.Favourites: return new TabFavourites_ContentView(); 
+                case Page.Gallery: return new TabGallery_ContentView(); 
+            }
+            return null;
+        }
+
+        private void LoadContent(Page newPage)
+        {
+            if (currentPage == newPage) return;
+            Togglebutton(currentPage, false);
+
+            currentPage = newPage;
+            Togglebutton(currentPage, true);
+
+            MyStackLayout.Children.Clear();
+            MyStackLayout.Children.Add(Create(currentPage));
+        }
+
+        private void Togglebutton(Page newPage, bool enabled)
+        {
+            Color newColor = enabled ? Color.White : Color.FromHex("#A648EB");
+            switch (newPage)
+            {
+                case Page.About: { ButtonAbout.TextColor = newColor; break; }
+                case Page.Favourites: { ButtonFavourites.TextColor = newColor; break; }
+                case Page.Gallery: { ButtonGallery.TextColor = newColor; break; }
+            }
+        }
         private void ButtonPress_About(object sender, EventArgs e)
         {
-            MyStackLayout.Children.Clear();
-            MyStackLayout.Children.Add(new TabAbout_ContentView());
+            LoadContent(Page.About);
         }
 
         private void ButtonPress_Favourites(object sender, EventArgs e)
         {
-            MyStackLayout.Children.Clear();
-            MyStackLayout.Children.Add(new TabFavourites_ContentView());
+            LoadContent(Page.Favourites);
         }
 
         private void ButtonPress_Gallery(object sender, EventArgs e)
         {
-            MyStackLayout.Children.Clear();
-            MyStackLayout.Children.Add(new TabGallery_ContentView());
+            LoadContent(Page.Gallery);
         }
     }
 }
