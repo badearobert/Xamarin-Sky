b920faf62d4e3614d1b71ed359c33c28e9c94037
Robert Badea <badea.robert@ymail.com>
2020-01-05 22:38:03 +0200

    Add changes to view when booking

--- Sky/Tab2_Discover/FullPlaceContentPage.xaml.cs
+++ Sky/Tab2_Discover/FullPlaceContentPage.xaml.cs
@@ -14,6 +14,7 @@ namespace Sky.Tab2_Discover
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FullPlaceContentPage : ContentPage
     {
+        private bool IsBooked = false;
         SwitchButtonViewModel viewModel = new SwitchButtonViewModel("tab2_discover_places_button_favorites_remove.png", "tab2_discover_places_button_favorites_add.png", false);
         public FullPlaceContentPage()
         {
@@ -25,7 +26,15 @@ namespace Sky.Tab2_Discover
 
         private void ButtonPressed_BookPlace(object sender, EventArgs e)
         {
-            PopupHelper.ShowAlertDialog("TravelBasePage", "Button pressed - Book place", this);
+            ToggleState();
+        }
+        private void ToggleState()
+        {
+            IsBooked = !IsBooked;
+            viewModel.Toggle();
+            MainDatePickerInfo.Text = IsBooked ? "BOOKED" : "SELECT A DATE";
+            BookingLabel.Text = IsBooked ? "CANCEL" : "BOOK THIS FOR $25.50";
+            BookingLabelParent.BackgroundColor = IsBooked ? Color.Gray : Color.Default;
         }
 
         private void ButtonPressed_DatePicker(object sender, EventArgs e)
@@ -33,13 +42,9 @@ namespace Sky.Tab2_Discover
             MainDatePicker.Focus();
         }
 
-        private void ButtonPressed_AddFavorite(object sender, EventArgs e)
-        {
-            PopupHelper.ShowAlertDialog("TravelBasePage", "Button pressed - Add favorite", this);
-        }
         private void SwitchPressedFavourite(object sender, EventArgs e)
         {
-            viewModel.Toggle();
+            // viewModel.Toggle();
         }
 
         private void MainDatePicker_DateSelected(object sender, DateChangedEventArgs e)
