55d8caf9fcb67602f4088ec2a9b5dbd7c10b0825
Robert Badea <badea.robert@ymail.com>
2020-01-12 11:58:37 +0200

    Added resource dictionary

---
+++ Sky/Common/CommonResourceDictionary.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sky.Common
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CommonResourceDictionary : ResourceDictionary
+    {
+        public CommonResourceDictionary()
+        {
+            InitializeComponent();
+        }
+    }
+}
